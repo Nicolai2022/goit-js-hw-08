bae4217f10b0b0dd96f7a9fd5d99938ebb8939a8
Nicolai2022 <98426734+Nicolai2022@users.noreply.github.com>
2023-02-14 14:04:01 +0000

    Deploying to gh-pages from @ Nicolai2022/goit-js-hw-08@62a91127808325619c48fb801732b3759c456fb7 🚀

---
+++ 03-feedback.fe6279f4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".feedback-form");t.addEventListener("input",throttle(onFormData,500)),t.addEventListener("submit",onSubmitForm)}();
+//# sourceMappingURL=03-feedback.fe6279f4.js.map
